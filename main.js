fa973e19eef1441a1138ace5c7adcb710f80fdc8
jaison joshi <jaisonjoshi2001@gmail.com>
2020-10-04 21:29:11 +0530

    set ayi

---
+++ main.js
@@ -0,0 +1,33 @@
+function test(card){
+    var id = card.id;
+    sessionStorage.setItem("cardid", id);
+    window.location = "info.html";
+}
+
+function getcard(){
+    var cardid = sessionStorage.getItem("cardid");
+   function makeajaxcall(callback){
+   
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','data.json');
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            var resp = JSON.parse(xhr.responseText);
+            callback(resp);
+        }
+    }
+    xhr.send();
+   }
+
+   function cardimp(data){
+       var container = document.querySelector('#cardcontainer');
+       for(let i =0;i<data.length;i++){
+           if(cardid == data[i].name){
+               let output = '<img src="'+data[i].src+'"><h2>'+data[i].name+'</h2>';
+               container.innerHTML = output;
+           }
+       }
+   }
+  
+makeajaxcall(cardimp);
+}
